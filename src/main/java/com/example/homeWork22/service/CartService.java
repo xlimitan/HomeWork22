17e2ead8b3e1116f9b6cc763239bfbce75e5c19d
xlimi <xlimitan@gmail.com>
2023-06-12 11:11:19 +0700

    DZ

---
+++ src/main/java/com/example/homeWork22/service/CartService.java
@@ -0,0 +1,23 @@
+package com.example.homeWork22.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+@Service
+@SessionScope
+public class CartService {
+    private final List<Integer> storage = new ArrayList<>();
+
+    public void add (List<Integer> ids) {
+        storage.addAll(ids);
+    }
+
+    public List<Integer> get() {
+        return Collections.unmodifiableList(storage);
+    }
+}
